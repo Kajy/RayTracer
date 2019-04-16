96ae44b559824ced52dce429748c3ef685a50275
Kevin Julien <kevin.julien@canal-plus.com>
2019-04-16 14:03:24 +0200

    add async func, not working

--- src/RayTracer/RayTracer.cpp
+++ src/RayTracer/RayTracer.cpp
@@ -19,8 +19,25 @@ void    RayTracer::run()
 {
     uint32_t width = _window.getDrawableSurfaceWidth();
     uint32_t height = _window.getDrawableSurfaceHeight();
+    std::size_t max = width * height;
+    uint32_t cores = std::thread::hardware_concurrency();
+    std::vector<std::future<void>> future_vector;
 
-    for (uint32_t x = 0; x < width; ++x) {
+    for (uint32_t i = 0; i < cores; ++i) {
+      future_vector.emplace_back(std::async(std::launch::async, [&]() {
+            for (std::size_t index(i); index < max; index += cores) {
+                uint32_t x = index % width;
+                uint32_t y = index / width;
+                if (this->_scene.renderScene(x, y)) {
+                    this->_window.drawPixel(WHITE_COLOR, x, y);
+                } else {
+                    this->_window.drawPixel(BLACK_COLOR, x, y);
+                }
+            }
+        }));
+    }
+
+   /* for (uint32_t x = 0; x < width; ++x) {
         for (uint32_t y = 0; y < height; ++y) {
 			if (this->_scene.renderScene(x, y)) {
 				this->_window.drawPixel(WHITE_COLOR, x, y);
@@ -29,8 +46,9 @@ void    RayTracer::run()
                 this->_window.drawPixel(BLACK_COLOR, x, y);
             }
         }
-    }
+    }*/
 
+   //std::this_thread::sleep_for(std::chrono::seconds(3));
     this->_window.render();
 
     while (!(_eventHandler.mayClose())) {
