f1b014be5279d93d5db0a77d86d5bf204d1986ad
Julian Vives <vivesjulian@gmail.com>
2019-04-22 22:01:09 +0200

    ADD SDL2_image, export png, edited the way how we fill pixels

--- src/Window/Window.cpp
+++ src/Window/Window.cpp
@@ -2,6 +2,7 @@
 // Created by xw on 12/04/19.
 //
 
+#include <SDL2/SDL_image.h>
 #include "Window.hpp"
 
 Window::Window(int w, int h):
@@ -28,7 +29,7 @@ Window::Window(int w, int h):
     _image = new Intersection[_drawableSurfaceHeight * _drawableSurfaceWidth];
     _distanceMap = new Intersection[_drawableSurfaceHeight * _drawableSurfaceWidth];
 
-    if ((_renderer = std::unique_ptr<SDL_Renderer, SDLRendererDeleter>(SDL_CreateRenderer(_window.get(), -1, SDL_RENDERER_ACCELERATED))) == nullptr) {
+    if ((_renderer = std::unique_ptr<SDL_Renderer, SDLRendererDeleter>(SDL_CreateRenderer(_window.get(), -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_TARGETTEXTURE))) == nullptr) {
         Debug::printError("Error when creating renderer");
     }
 }
@@ -49,16 +50,42 @@ void        Window::drawPixel(Intersection inter, uint32_t x, uint32_t y) const
     _image[y * _drawableSurfaceWidth + x].distanceWithViewer = inter.distanceWithViewer;
 }
 
+static void savePNG(SDL_Texture *texture, uint32_t *pixels, uint32_t w, uint32_t h)
+{
+    Uint32 format_pixels;
+    SDL_QueryTexture(texture, &format_pixels, NULL, NULL, NULL);
+    SDL_Surface *surface = SDL_CreateRGBSurfaceWithFormatFrom(pixels, w, h, 32, w * sizeof(Uint32), format_pixels);
+    IMG_SavePNG(surface, "scene.png");
+    SDL_FreeSurface(surface);
+}
+
 void        Window::render() const
 {
+
+    SDL_Renderer *renderer = _renderer.get();
+    void *tmp;
+    Uint32 *pixels;
+    int pitch;
+    SDL_Texture *texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING,
+                                _drawableSurfaceWidth, _drawableSurfaceHeight);
+    SDL_PixelFormat *format = SDL_AllocFormat(SDL_PIXELFORMAT_RGBA8888);
+    SDL_LockTexture(texture, NULL, &tmp, &pitch);
+    pixels = static_cast<Uint32 *>(tmp);
+
     for (uint32_t x = 0; x < _drawableSurfaceWidth; ++x) {
         for (uint32_t y = 0; y < _drawableSurfaceHeight; ++y) {
-            Color color = _distanceMap[y * _drawableSurfaceWidth + x].color;
-            SDL_SetRenderDrawColor(_renderer.get(), color.red, color.green, color.blue, 255);
-            SDL_RenderDrawPoint(_renderer.get(), x, y);
+            Color color = _image[y * _drawableSurfaceWidth + x].color;
+            pixels[y * _drawableSurfaceWidth + x] = SDL_MapRGBA(format, color.red, color.blue, color.green, color.alpha);
         }
     }
-    SDL_RenderPresent(_renderer.get());
+
+    SDL_FreeFormat(format);
+    SDL_UnlockTexture(texture);
+    SDL_RenderCopy(renderer, texture, NULL, NULL);
+    SDL_RenderPresent(renderer);
+
+    savePNG(texture, pixels, _drawableSurfaceWidth, _drawableSurfaceHeight);
+    SDL_DestroyTexture(texture);
 }
 
 void        Window::generateDistanceMap(double maxDist) {
@@ -84,4 +111,16 @@ int         Window::getDrawableSurfaceHeight() const {
 
 Intersection    *Window::getImage() const {
     return _image;
+}
+
+const std::unique_ptr<SDL_Window, SDLWindowDeleter> &Window::getWindow() const {
+    return _window;
+}
+
+const std::unique_ptr<SDL_Renderer, SDLRendererDeleter> &Window::getRenderer() const {
+    return _renderer;
+}
+
+const std::unique_ptr<SDL_Texture, SDLTextureDeleter> &Window::getTexture() const {
+    return _texture;
 }
